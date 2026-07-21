83f51a935db5a620ad4ba7f448625edac9484f33
Duy Nguyen <phuongduy10tin@gmail.com>
2026-07-22 00:14:42 +0700

    Session 11

---
+++ BaiTap/Buoi11/index.js
@@ -0,0 +1,41 @@
+//Bài 1
+let a = 10;
+let b = 3;
+
+// Viết code ở đây
+console.log("Tổng:", a + b);
+console.log("Hiệu:", a - b);
+console.log("Tích:", a * b);
+console.log("Thương:", a / b);
+
+//Bai 2
+let chieuDai = 10;
+let chieuRong = 5;
+
+// Viết code ở đây
+console.log("Dien tich: ", chieuDai * chieuRong);
+console.log("Chu vi :", (chieuDai + chieuRong) * 2);
+
+//Bài 3
+
+let soN = 7;
+
+// Viết code ở đây
+console.log("soN mod 2 là : ", soN % 2);
+
+//Bài 3
+
+let doC = 25;
+// Viết code ở đây
+console.log("doF la : ", (doC * 9) / 5 + 32);
+doC += 5;
+console.log("doF la : ", (doC * 9) / 5 + 32);
+
+//Bai 5
+let soKwh = 150;
+
+// Viết code ở đây
+console.log(
+    "Tien dien la : ",
+    soKwh <= 100 ? soKwh * 1500 : (soKwh - 100) * 2000 + 100 * 1500,
+);
